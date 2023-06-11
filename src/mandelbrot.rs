2b90149e691bb26b5054c2a3ffb41c50e0f13e18
Itamar Schwartz <itamarschwartz10@gmail.com>
2023-06-11 23:13:19 +0300

    Cardioid and bulb check

--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -11,6 +11,11 @@ pub fn mandelbrot_iterations(c: Complex) -> u32 {
         re: 0f64,
         img: 0f64,
     };
+
+    if cardioid_and_bulb_check(c) {
+        return MAX_ITERATIONS;
+    }
+
     loop {
         z0 = z0 * z0 + c;
         if z0.mag() > (2f64) || iterations == MAX_ITERATIONS {
@@ -18,6 +23,14 @@ pub fn mandelbrot_iterations(c: Complex) -> u32 {
         }
         iterations += 1;
     }
+}
+
+fn cardioid_and_bulb_check(c: Complex) -> bool {
+    let c_img_sqr = c.img * c.img;
+    let c_mag_sqr = c.re * c.re + c_img_sqr;
+
+    return (c.re + 1.0) * (c.re + 1.0) + c_img_sqr <= 0.0625f64
+        || c_mag_sqr * (8.0f64 * c_mag_sqr - 3.0) <= 0.09375 - c.re;
 }
 
 pub fn mandelbrot_color(zoom: f64, x: f64, y: f64) -> Hsv<f32> {
